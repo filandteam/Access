e227e8eda6c555ff6e8159d8c519b85fc7fee542
PoltavaAndrey <50247367+PoltavaAndrey@users.noreply.github.com>
2019-08-09 15:59:47 +0300

    First realse

---
+++ ADB/ADB/cSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Саша, этот класс твой
+
+namespace ADB
+{
+    partial class cSystem
+    {
+        partial void OpenDB();
+    }
+}
---
+++ ADB/ADB/cSystem1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+// Не трогать!!!
+
+namespace ADB
+{
+    partial class cSystem
+    {
+        public System.Data.OleDb.OleDbConnection _Connection;
+        public cSystem()
+        {
+
+        }
+
+        ~cSystem()
+        {
+
+        }
+
+        partial void OpenDB()
+        {
+            using (System.Data.OleDb.OleDbConnection connection = new System.Data.OleDb.OleDbConnection()) {
+                connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+                    @"Data source= ADB.mdb"; // Пока только пример. Надо будет менять путь.
+                _Connection = connection;
+            }
+        }
+    }
+}
